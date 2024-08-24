e7b20b522ea782ba767e1ee14cd67c1c92fcb8c3
AGARWALMANU3008 <manuagarwal3008@gmail.com>
2024-08-25 01:43:57 +0530

    css error resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
